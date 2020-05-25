5d42c36765837fddf6a1c0eed0e166d4c42fb9d1
Максим Матуляк <maksymmatuliak@gmail.com>
2020-05-25 10:54:07 +0300

    rewrote authenticationService

--- src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
+++ src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
@@ -14,11 +14,13 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
-        User userFromDB = userService.findByEmail(email);
-        if (userFromDB == null) {
+        User userFromDB;
+        try {
+            userFromDB = userService.findByEmail(email);
+        } catch (Exception e) {
             throw new AuthenticationException("Incorrect email or password");
         }
-        if (userFromDB.getPassword().equals(
+        if (userFromDB != null && userFromDB.getPassword().equals(
                 HashUtil.hashPassword(password, userFromDB.getSalt()))) {
             return userFromDB;
         }
