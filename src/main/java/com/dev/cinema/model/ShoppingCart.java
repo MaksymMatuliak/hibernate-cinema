36633c2ec71356e462e1a60d33bb33fca92d01d5
Максим Матуляк <maksymmatuliak@gmail.com>
2020-05-26 15:29:23 +0300

    changed naming in shopping cart table

--- src/main/java/com/dev/cinema/model/ShoppingCart.java
+++ src/main/java/com/dev/cinema/model/ShoppingCart.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -23,6 +24,7 @@ public class ShoppingCart {
 
     @OneToOne
     @MapsId
+    @JoinColumn(name = "shopping_cart_id")
     private User user;
 
     public Long getShoppingCartId() {
