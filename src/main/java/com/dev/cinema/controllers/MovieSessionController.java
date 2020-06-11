5c3281e8826261767a029b9bacbeb40bb44ae341
Максим Матуляк <maksymmatuliak@gmail.com>
2020-06-11 20:12:07 +0300

    deleted unused import

--- src/main/java/com/dev/cinema/controllers/MovieSessionController.java
+++ src/main/java/com/dev/cinema/controllers/MovieSessionController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
