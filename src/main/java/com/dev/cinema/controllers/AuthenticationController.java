390f8af8458597c1a3bb3e84e627cf15d8f94cef
Максим Матуляк <maksymmatuliak@gmail.com>
2020-06-15 13:47:30 +0300

    fixed style

--- src/main/java/com/dev/cinema/controllers/AuthenticationController.java
+++ src/main/java/com/dev/cinema/controllers/AuthenticationController.java
@@ -3,7 +3,6 @@ package com.dev.cinema.controllers;
 import com.dev.cinema.model.dto.UserRequestDto;
 import com.dev.cinema.security.AuthenticationService;
 import javax.validation.Valid;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
